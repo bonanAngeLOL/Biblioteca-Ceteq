2ed7e73ee6c78d9d09123e1be15a773e2a2cfdbd
bonanAngeLOL <angellopezcue@gmail.com>
2021-02-08 12:03:43 -0600

    AutoresModel added

--- src/main/java/com/ceteq/biblioteca/model/AutoresModel.java
+++ src/main/java/com/ceteq/biblioteca/model/AutoresModel.java
@@ -1,5 +1,97 @@
 package com.ceteq.biblioteca.model;
 
-public class AutoresModel {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "b_autores")
+public class AutoresModel implements Serializable{
+
+	private static final long serialVersionUID = 446206550955721929L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_autor")
+	private int idAutor;
+	
+	@Column(name = "nombre", length = 50, nullable = false)
+	private String nombre;
+	
+	@Column(name = "apellidos", length = 50, nullable = false)
+	private String apellidos;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name  = "fecha_nacimiento", nullable = false)
+	private Date fechaNacimiento;
+
+	@Column(name = "nacionalidad")
+	private String nacionalidad;
+
+	public int getIdAutor() {
+		return idAutor;
+	}
+
+	public void setIdAutor(int idAutor) {
+		this.idAutor = idAutor;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public AutoresModel(int idAutor, String nombre, String apellidos, Date fechaNacimiento, String nacionalidad) {
+		super();
+		this.idAutor = idAutor;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechaNacimiento = fechaNacimiento;
+		this.nacionalidad = nacionalidad;
+	}
+
+	public AutoresModel(int idAutor) {
+		super();
+		this.idAutor = idAutor;
+	}
+	
+	public AutoresModel() {
+		super();
+	}
+	
 }
