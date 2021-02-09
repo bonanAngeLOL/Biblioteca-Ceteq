9e789ac2c817a968b4ec046d842d523e6db59b71
bonanAngeLOL <angellopezcue@gmail.com>
2021-02-09 14:09:42 -0600

    Usuario Controller added

---
+++ src/main/java/com/ceteq/biblioteca/controller/UsuarioController.java
@@ -0,0 +1,57 @@
+package com.ceteq.biblioteca.controller;
+
+import java.util.List;
+
+import com.ceteq.biblioteca.bean.UsuarioBean;
+import com.ceteq.biblioteca.service.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController{
+
+    @Autowired
+    private UsuarioService usuarioServ;
+
+    @PostMapping
+    public ResponseEntity<UsuarioBean> create(@RequestBody UsuarioBean usuario){
+        usuario.setIdUsuario(0);
+        usuarioServ.save(usuario);
+        return new ResponseEntity<UsuarioBean>(usuario, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioBean>> getAll(){
+        List<UsuarioBean> usuarios = usuarioServ.findAll();
+        return new ResponseEntity<List<UsuarioBean>>(usuarios, HttpStatus.OK);
+    }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<UsuarioBean> findById(int idUsuario){
+        UsuarioBean user = usuarioServ.findById(idUsuario);
+        return new ResponseEntity<UsuarioBean>(user, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<UsuarioBean> update(UsuarioBean usuario){
+        usuarioServ.save(usuario);
+        return new ResponseEntity<UsuarioBean>(usuario, HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Boolean> delete(int idUsuario){
+        usuarioServ.deleteById(idUsuario);
+        return new ResponseEntity<Boolean>(true, HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/ceteq/biblioteca/serviceImpl/UsuarioServiceImpl.java
+++ src/main/java/com/ceteq/biblioteca/serviceImpl/UsuarioServiceImpl.java
@@ -3,13 +3,18 @@ package com.ceteq.biblioteca.serviceImpl;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import com.ceteq.biblioteca.bean.UsuarioBean;
 import com.ceteq.biblioteca.model.UsuarioModel;
 import com.ceteq.biblioteca.repository.UsuarioRepository;
 import com.ceteq.biblioteca.service.UsuarioService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
+@Transactional
 public class UsuarioServiceImpl implements UsuarioService{
 
     @Autowired
@@ -26,16 +31,17 @@ public class UsuarioServiceImpl implements UsuarioService{
         List<UsuarioModel> usuarios = usuarioRepo.findAll();
         List<UsuarioBean> usuariosBean = new ArrayList<>();
         for(UsuarioModel usuario : usuarios) {
-            UsuarioBean usuarioBean = new UsuarioBean();
-            usuarioBean.setIdUsuario(usuario.getIdUsuario());
-            usuarioBean.setNombre(usuario.getNombre());
-            usuarioBean.setApellidos(usuario.getApellidos());
-            usuarioBean.setCurp(usuario.getCurp());
-            usuarioBean.setFechaNacimiento(usuario.getFechaNacimiento());
-            usuarioBean.setTelefono(usuario.getTelefono());
-            
-            usuarioBean.setDireccion(usuario.getDirecion());
-            usuarioBean.setEmail(usuario.getCorreo());
+            UsuarioBean usuarioBean = new UsuarioBean(
+                    usuario.getIdUsuario(), 
+                    usuario.getNombre(), 
+                    usuario.getApellidos(),
+                    usuario.getCurp(), 
+                    usuario.getFechaNacimiento(),
+                    usuario.getTelefono(), 
+                    usuario.getDirecion(), 
+                    usuario.getCorreo()
+            );
+            usuariosBean.add(usuarioBean);
         }
         return usuariosBean;
     }
@@ -43,18 +49,17 @@ public class UsuarioServiceImpl implements UsuarioService{
     @Override
     public UsuarioBean findById(int idUsuario) {
         UsuarioModel usuario = usuarioRepo.findById(idUsuario).orElseThrow();
-        UsuarioBean userBean = new UsuarioBean();
-
-        userBean.setIdUsuario(usuario.getIdUsuario());
-        userBean.setNombre(usuario.getNombre());
-        userBean.setApellidos(usuario.getApellidos());
-        userBean.setCurp(usuario.getCurp());
-        userBean.setFechaNacimiento(usuario.getFechaNacimiento());
-        userBean.setTelefono(usuario.getTelefono());
-        userBean.setDireccion(usuario.getDirecion());
-        userBean.setEmail(usuario.getCorreo());
-
-        return userBean;
+        UsuarioBean usuarioBean = new UsuarioBean(
+                usuario.getIdUsuario(), 
+                usuario.getNombre(), 
+                usuario.getApellidos(),
+                usuario.getCurp(), 
+                usuario.getFechaNacimiento(),
+                usuario.getTelefono(), 
+                usuario.getDirecion(), 
+                usuario.getCorreo()
+        );
+        return usuarioBean;
     }
 
     @Override
